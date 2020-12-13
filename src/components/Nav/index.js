2ee820d502180a90470aa56b8b047cbf3c586690
daejo <daejo@icloud.com>
2020-12-12 23:28:12 -0800

    fixes navigation coding

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 
 function Nav(props) {
+    const {
+        pages = [],
+        setCurrentPage,
+        currentPage,
+        contactSelected,
+        setContactSelected
+      } = props;
+    
     return (
+        
         <nav>
             <ul className="nav_links">
                 <li>
-                    <a href="#">This is the nav</a>
+                    <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+                        About me
+                    </a>
                 </li>
-                <li>
-                    <a href="#">This is the nav</a>
+                <li className={`${contactSelected && 'navActive'}`}>
+                    <span onClick={() => setContactSelected(true)}>
+                    Contact
+                    </span>
                 </li>
-                <li>
-                    <a href="#">This is the nav</a>
-                </li>
+                {pages.map((page) => (
+                    <li className={`mx-1 ${
+                        currentPage.name === page.name && !contactSelected && `navActive`}`} key={page.name}>
+                    <span onClick={() => {
+                        setCurrentPage(page);
+                        setContactSelected(false);}}>                            
+                    </span>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
