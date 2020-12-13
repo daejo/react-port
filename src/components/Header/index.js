38b21d9ab9d9f6d4750bf762c08263b18d2c963c
daejo <daejo@icloud.com>
2020-12-12 22:35:13 -0800

    adds background and header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from '../Nav'
 
 function Header(props) {
     return (
         <header>
             This is the header
+            <Nav />
         </header>
     );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nav(props) {
+    return (
+        <nav>
+            This is the nav
+        </nav>
+    );
+}
+
+export default Nav;
